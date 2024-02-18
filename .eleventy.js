03df1780e422c62bba0af429af5152f22abfb285
Roman Waters <watersstewartr@gmail.com>
2024-02-18 06:15:30 +0000

    initial site

---
+++ .eleventy.js
@@ -0,0 +1,39 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(`./src${src}`, {
+    widths: [300, 800, null],
+    formats: ["avif", "jpeg"],
+    urlPath: "/images/",
+    outputDir: "./public/images/"
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async"
+  };
+
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/assets/css/");
+  eleventyConfig.addWatchTarget("./src/assets/css/");
+  eleventyConfig.addPassthroughCopy("./src/assets/images/");
+  eleventyConfig.addPassthroughCopy({ "./src/assets/favicons": "/" });
+
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  eleventyConfig.addNunjucksAsyncShortcode("EleventyImage", imageShortcode);
+
+
+    return {
+        dir: {
+            input: "src",
+            output: "public",
+        
+        },
+    };
+};
