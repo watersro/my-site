25f3831730227f8f17ead1b67c9d188d374e6af3
Roman Waters <hello@romanwaters.com>
2024-04-26 16:29:40 +0100

    process page mvp

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
     },
     animation: {
       "pulse-slow": "pulse 4s linear infinite",
+      pulse: "pulse 3s linear infinite",
     },
   },
   plugins: [require("@tailwindcss/typography")],
