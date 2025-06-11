fbe6f3139dd93b30dc8d68fab4455ee5160fd82e
Roman Waters <hello@romanwaters.com>
2025-06-11 22:35:56 +0100

    canonicalised fix

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,5 @@
 const isDev = process.env.ELEVENTY_ENV === "development";
 
 module.exports = {
-  baseUrl: isDev
-    ? "http://localhost:8080/" // include protocol + trailing slash
-    : "https://www.romanwaters.com/",
+  baseUrl: isDev ? "http://localhost:8080/" : "https://romanwaters.com/",
 };
